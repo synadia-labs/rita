b4d8ef5d322bb965b4dbccee8c1f19e04f4c865e
Jordan Rash <jordan@synadia.com>
2025-12-03 16:50:03 -0700

    adds test for decide & evolve for model
    Signed-off-by: Jordan Rash <jordan@synadia.com>

--- model_test.go
+++ model_test.go
@@ -1,6 +1,61 @@
 package rita
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/nats-io/nats.go"
+	"github.com/synadia-labs/rita/testutil"
+	"github.com/synadia-labs/rita/types"
+)
+
+var (
+	_ Decider = (*OrderStats)(nil)
+	_ Evolver = (*OrderStats)(nil)
+)
+
+func TestModelDecideEvolve(t *testing.T) {
+	is := testutil.NewIs(t)
+
+	srv := testutil.NewNatsServer()
+	defer testutil.ShutdownNatsServer(srv)
+
+	nc, err := nats.Connect(srv.ClientURL())
+	is.NoErr(err)
+
+	tr, err := types.NewRegistry(registry)
+	is.NoErr(err)
+
+	m, err := New(nc, WithRegistry(tr))
+	is.NoErr(err)
+
+	ctx := context.Background()
+	es, err := m.CreateEventStore(ctx, EventStoreConfig{
+		Name: "store",
+	})
+	is.NoErr(err)
+
+	cmd := &Command{
+		Data: &PlaceOrder{},
+	}
+
+	state := &OrderStats{}
+	stateModel := NewModel(state)
+
+	evt, err := stateModel.Decide(cmd)
+	is.NoErr(err)
+	is.Equal(state.Decisions, 1)
+
+	seq, err := es.Append(ctx, evt)
+	is.NoErr(err)
+	is.Equal(seq, uint64(1))
+
+	var events eventSlice
+	_, err = es.Evolve(ctx, &events)
+	is.NoErr(err)
+	is.Equal(len(events), 1)
+	is.Equal(events[0].Type, "order-placed")
+}
 
 type Profile struct{}
 
