45c5c9627c27192061c84245e6705343e74eef6f
Dan Sultanowsky <dan.sultanowsky@synadia.com>
2026-05-13 14:09:59 +1000

    Defensive checks in test

--- reactor_test.go
+++ reactor_test.go
@@ -86,7 +86,7 @@ func TestReact_WithoutCreateReturnsNotFound(t *testing.T) {
 	if !errors.Is(err, ErrReactorNotFound) {
 		t.Fatalf("expected ErrReactorNotFound, got %v", err)
 	}
-	if errors.Is(err, jetstream.ErrConsumerNotFound) {
+	if errors.Is(err, jetstream.ErrConsumerNotFound) || errors.Is(err, jetstream.ErrConsumerDoesNotExist) {
 		t.Fatalf("expected JetStream sentinel to stay internal, got %v", err)
 	}
 }
